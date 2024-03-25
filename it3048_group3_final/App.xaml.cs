0286efc018b23348ba3455d12438ff3885503626
gracenordwick <92000737+gracenordwick@users.noreply.github.com>
2024-03-25 01:53:42 -0400

    Add project files.

---
+++ it3048_group3_final/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace it3048_group3_final
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
