f93d237fc6727956d10ac57f3b572216c2e41fae
gracenordwick <92000737+gracenordwick@users.noreply.github.com>
2024-04-02 03:07:47 -0400

    graces changes

--- it3048_group3_final/App.xaml.cs
+++ it3048_group3_final/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using it3048_group3_final.Data;
+using it3048_group3_final.Views;
+using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,12 +9,26 @@ namespace it3048_group3_final
 {
     public partial class App : Application
     {
+        private static CalendarItemDatabase database;
+        internal static CalendarItemDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new CalendarItemDatabase(Path.Combine(Environment.GetFolderPath
+                    (Environment.SpecialFolder.LocalApplicationData), "CalendarItems.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new CalendarView());
         }
+
 
         protected override void OnStart()
         {
