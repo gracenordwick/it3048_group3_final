3d5efce5c37db89706bae33b0f84851f770b4b3b
noahwardega <130424011+noahwardega@users.noreply.github.com>
2024-04-09 23:41:13 -0400

    tasks save to database and database items display in box

--- it3048_group3_final/Views/AddItemPage.xaml.cs
+++ it3048_group3_final/Views/AddItemPage.xaml.cs
@@ -8,6 +8,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using it3048_group3_final.Models;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace it3048_group3_final.Views
 {
@@ -17,6 +18,26 @@ namespace it3048_group3_final.Views
         {
             InitializeComponent();
         }
+
+        async void OnAddClicked(object sender, EventArgs e)
+        {
+            var calendarItem = new CalendarItem
+            {
+                Date = datePicker.Date,
+                Title = titleEntry.Text,
+                Time = timePicker.Time
+            };
+
+            // Save the new calendar item to the database
+            await App.Database.SaveItemAsync(calendarItem);
+
+            // Log the saved item ID for debugging
+            Console.WriteLine("Saved item ID: " + calendarItem.ID);
+
+            // Navigate back to the CalendarView page
+            await Navigation.PopAsync();
+        }
+
 
         async void OnSaveClicked(object sender, EventArgs e)
         {
--- it3048_group3_final/Views/CalendarView.xaml.cs
+++ it3048_group3_final/Views/CalendarView.xaml.cs
@@ -11,13 +11,16 @@ using it3048_group3_final.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace it3048_group3_final.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CalendarView : ContentPage, INotifyPropertyChanged
 	{
-		private DateTime _currentDate;
+        public ObservableCollection<CalendarItem> CalendarItems { get; set; }
+
+        private DateTime _currentDate;
 
 		public DateTime CurrentDate 
 		{
@@ -32,16 +35,41 @@ namespace it3048_group3_final.Views
 			}
 		}
 
-		public CalendarView()
-		{
-			InitializeComponent();
-			CurrentDate = DateTime.Today;
-			BindingContext = this;
-		}
+        public CalendarView()
+        {
+            InitializeComponent();
+            CalendarItems = new ObservableCollection<CalendarItem>();
+            BindingContext = this;
+            LoadItems();
+        }
+
+        private async void LoadItems()
+        {
+            // Fetch calendar items from the database
+            var items = await App.Database.GetItemsAsync();
+
+            // Clear existing items and add fetched items
+            CalendarItems.Clear();
+            foreach (var item in items)
+            {
+                CalendarItems.Add(item);
+                /*if (item.Date.Date == CurrentDate.Date) // Comparing only the date part
+                {
+                    CalendarItems.Add(item);
+                }*/
+            }
+        }
 
         private async void OnAddTaskClicked(object sender, EventArgs e)
         {
+            // Navigate to the AddItemPage
             await Navigation.PushAsync(new AddItemPage());
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            LoadItems();
         }
 
     }
