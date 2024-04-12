3e2c7972754e2f036ffe1b880b69aa4d1ee866cd
noahwardega <130424011+noahwardega@users.noreply.github.com>
2024-04-12 01:44:08 -0400

    Update AddItemPage.xaml.cs to match noahs

--- it3048_group3_final/Views/AddItemPage.xaml.cs
+++ it3048_group3_final/Views/AddItemPage.xaml.cs
@@ -8,29 +8,46 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using it3048_group3_final.Models;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace it3048_group3_final.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddItemPage : ContentPage
     {
         public AddItemPage()
         {
             InitializeComponent();
         }
-        async void onSaveClicked()
+
+        async void OnAddClicked(object sender, EventArgs e)
         {
-            var calendarItem = (CalendarItem)BindingContext;
-            await App.Database.SaveItemsAsync(calendarItem);
+            var calendarItem = new CalendarItem
+            {
+                Date = datePicker.Date,
+                Title = titleEntry.Text,
+                Time = timePicker.Time
+            };
+
+            // Save the new calendar item to the database
+            await App.Database.SaveItemAsync(calendarItem);
+
+            // Log the saved item ID for debugging
+            Console.WriteLine("Saved item ID: " + calendarItem.ID);
+
+            // Navigate back to the CalendarView page
             await Navigation.PopAsync();
         }
-        async void onDeleteClicked()
+
+
+        async void OnSaveClicked(object sender, EventArgs e)
         {
             var calendarItem = (CalendarItem)BindingContext;
-            await App.Database.DeleteItemsAsync(calendarItem);
+            await App.Database.SaveItemAsync(calendarItem);
             await Navigation.PopAsync();
         }
-        async void onCancelClicked()
+
+
+        async void OnCancelClicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
         }
